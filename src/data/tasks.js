cab7d22dea8936067b7447deeda2efb0e529038c
unknown <ing.ramirozs@gmail.com>
2022-09-11 22:57:01 -0500

    proyecto terminado

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "tarea",
+    descciption: "matematicas",
+  },
+  {
+    id: 1,
+    title: "tarea2",
+    descciption: "programacion",
+  },
+  {
+    id: 2,
+    title: "tarea",
+    descciption: "deploy",
+  },
+];
